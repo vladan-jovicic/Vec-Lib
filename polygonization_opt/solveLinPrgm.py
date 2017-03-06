6f4191ca89665cdae17c9f0a0f1dc3b013701867
qguilman <qguilman@ens-lyon.fr>
2017-03-06 09:38:48 +0100

    polygonization: still not works but for theorical reasons

--- polygonization_opt/solveLinPrgm.py
+++ polygonization_opt/solveLinPrgm.py
@@ -69,6 +69,17 @@ def transpose(M):
 	m=len(M[0])
 	return [[M[i][j] for i in range(n)] for j in range(m)]
 
+def in01constraints(n):
+	#return the list of constraints corresponding to the fact that variables are in [0,1]
+	#there are n+n^2 variables
+	def indic(i,j):
+		if i==j:
+			return 1
+		else:
+			return 0
+
+	return [[indic(i,j) for j in range(n*(n+1))] for i in range(n*(n+1))]+[[-indic(i,j) for j in range(n*(n+1))] for i in range(n*(n+1))]
+
 def solvePdelta(convexList,delta):
 	#function to use when you know what distance you want, at most, between each pixel and an edge (the closest one). It compute the better number of point you can use (the min).	 
 
@@ -77,24 +88,35 @@ def solvePdelta(convexList,delta):
 
 	#first create the matrix
 	#we have |convexList|(|convexList|+1) variables all the x_e and z_{f,f'} that must stand for z_{f,f'}=x_f,x_f'.
-	coefs=computeAPdelta(convexList,delta) #all th coef of the form a_{e,f,f'}
+	coefsA=computeAPdelta(convexList,delta) #all th coef of the form a_{e,f,f'}
 	nulConvexList = [0 for e in convexList] #a list of coef 0 for all variable x_e
 
 	#we have to add nulConvexList at the beginning of each ligne e of coefs
-	coefs = concat(nulConvexList,coefs)
+	coefsA = concat(nulConvexList,coefsA)
 
 	#now we have to add the constraints for z_f,f' <= x_f and x_f'
 	nulProductVar = [0 for i in range(n**2)]
 	constraintZ = generateConstraintsZ(n)
 
-	A=matrix(transpose(coefs+constraintZ))
+	coefsA = coefsA+constraintZ
+	coefsB = [epsilon for i in range(n)]+[0 for i in range(2*(n**2))]
+	coefsC = [1. for e in convexList]+[(-1.) for i in range(n**2)]
+
+	#now add the constraint that each variable is in [0;1]
+	coefsA = coefsA+in01constraints(n)
+	coefsB = coefsB+[1 for i in range(n*(n+1))]+[0 for i in range(n*(n+1))]
+
+	#create matrice A
+	A=matrix(transpose(coefsA))
 
 	#now get vectors b and c
-	c=matrix([1. for e in convexList]+[(-1.) for i in range(n**2)]) #objective function
-	b=matrix([epsilon for i in range(n)]+[0 for i in range(2*(n**2))])
+	c=matrix(coefsC) #objective function
+	b=matrix(coefsB)
+
+	
 
 	sol=solvers.lp(c,A,b)
-	return sol['x']
+	return sol
 	
 
 def solvePnum(convexList,num):
